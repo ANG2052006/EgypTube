3dd122ee58331461753e83860923027c89f896b2
Adham <adhambnsayed2005hanafi@gmail.com>
2025-04-30 09:14:48 +0300

    Update firebase-upload.js

--- firebase-upload.js
+++ firebase-upload.js
@@ -1,7 +1,25 @@
 
 // Firebase configuration (استبدل البيانات بتاعتك من Firebase Console)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAoxtJnu-hh2u73P1ib2Z4t2Hmgxi6xjJA",
+    authDomain: "egyptube-12099.firebaseapp.com",
+    projectId: "egyptube-12099",
+    storageBucket: "egyptube-12099.firebasestorage.app",
+    messagingSenderId: "1039610895588",
+    appId: "1:1039610895588:web:0f9e6e97c4bfbb9fe6e0f1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
